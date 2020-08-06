ce5a3dbab6e7823c2baadcb54fe044c546d0152a
Elisa33 <pipi.tdfuego@gmail.com>
2020-08-06 17:08:37 -0300

    initial

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import '../styles/card.css';
+
+
+
+class Card extends Component{
+    render(){
+        return(
+            <div>
+                <img src='https://raw.githubusercontent.com/devoffarg/DevOff-Desafio-1/master/fotos/01.jpg' />
+            </div>
+        )
+    }
+}
+
+export default Card;
---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from "react"; 
+
+class Header extends React.Component {
+
+render() { return (
+
+  <header>
+    <div className="logo">
+      <h1>Dev-Off Gallery</h1>
+    </div>
+  </header>
+);
+} }
+
+export default Header;
